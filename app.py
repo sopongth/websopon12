0769308ecc9dd493105c88902ca4af626c0ead0b
sopongth <104857667+sopongth@users.noreply.github.com>
2022-08-11 23:13:40 +0700

    code v19

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def handle_text_message(event):
         client.connect(broker_address, port=port)
         client.subscribe("@msg/#")
         client.loop_start()
-        time.sleep(4) 
+        time.sleep(0.5) 
         client.loop_stop()
     except:
         print("Connection failed")
